1b97ff6b1e1699a1a30002752cb0e27476a6d61b
trananh91 <tranducanh717@gmail.com>
2022-10-08 10:32:40 +0700

    main

---
+++ src/uet/oop/bomberman/entities/Balloom.java
@@ -0,0 +1,19 @@
+package uet.oop.bomberman.entities;
+
+import javafx.scene.image.Image;
+
+public class Balloom extends Entity {
+    public Balloom(int x, int y, Image img) {
+        super(x, y, img);
+    }
+
+    @Override
+    public void update() {
+        x += 2;
+    }
+
+    @Override
+    public void checkCollide(Entity entity) {
+
+    }
+}
---
+++ src/uet/oop/bomberman/entities/Grass.java
@@ -0,0 +1,18 @@
+package uet.oop.bomberman.entities;
+
+import javafx.scene.image.Image;
+
+public class Grass extends Entity {
+
+    public Grass(int x, int y, Image img) {
+        super(x, y, img);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void checkCollide(Entity entity) {}
+}
